5f0651cb286d107c4f9cbd6d340facebae941b18
g0sek <gosekkamil@gmail.com>
2024-05-27 00:07:01 +0200

    api connection, store currencies list in combo boxes

--- src/main/java/com/example/currency_converter/Controller.java
+++ src/main/java/com/example/currency_converter/Controller.java
@@ -1,8 +1,112 @@
 package com.example.currency_converter;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
+    // currencyOne, CurrencyTwo - holds the currency tickers (from and to)
+    // apiKey - hold the api key to use the api
+    private String currencyOne, currencyTwo, apiKey;
 
+    private ArrayList<String> currencyList;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private TextField enterAmountField;
+
+    @FXML
+    private ComboBox<String> currencyOneBox, currencyTwoBox;
+
+    @FXML
+    private Label resultLabel;
+
+    public void initialize(){
+        // retrieve api key
+        getApiKey();
+
+        // load logo
+        loadLogo();
+
+        // retrieve currency list from api
+        try{
+            // retrieve and store list of currencies
+            currencyList = loadCurrencyList();
+
+            // store the list in the combo boxes
+            ObservableList<String> options = FXCollections.observableArrayList(currencyList);
+            currencyOneBox.setItems(options);
+            currencyTwoBox.setItems(options);
+
+        }catch(IOException e){
+            System.out.println("Error: Failed to load currency list from api " + e);
+        }
+    }
+
+    private void getApiKey(){
+        BufferedReader reader = null;
+        try{
+            String filePath = getClass().getResource("apikey.txt").getPath().replaceAll("%20", " ");
+            reader = new BufferedReader(new FileReader(filePath));
+
+            // store api key
+            apiKey = reader.readLine();
+
+        }catch(IOException e){
+            System.out.println("Error: " + e);
+        }finally {
+            // close the buffered reader object to free up resources
+            try{
+                if(reader != null) reader.close();
+            }catch (IOException e){
+                System.out.println("Error: " + e);
+            }
+        }
+    }
+
+    private void loadLogo(){
+        String logoPath = getClass().getResource("logoBanner.png").getPath().replaceAll("%20", " ");
+        logo.setImage(new Image(new File(logoPath).getAbsolutePath()));
+    }
+
+    private ArrayList<String> loadCurrencyList() throws IOException{
+        // retrieve current list through api call
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+        Request request = new Request.Builder()
+                .url("https://api.apilayer.com/currency_data/list")
+                .addHeader("apikey", apiKey)
+                .method("GET", null)
+            .build();
+        Response response = client.newCall(request).execute();
+
+        Gson gson = new Gson();
+        JsonElement jsonElement = gson.fromJson(response.body().charStream(), JsonElement.class);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+        ArrayList<String> currencyList = new ArrayList<>();
+        currencyList.addAll(jsonObject.getAsJsonObject("currencies").keySet());
+        return currencyList;
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example.currency_converter {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires okhttp3;
+    requires com.google.gson;
 
     opens com.example.currency_converter to javafx.fxml;
     exports com.example.currency_converter;
